ed030ff28718952d2a5c59674507b47fbc2469a7
Russ Nealis <codemunkee@gmail.com>
2017-10-14 18:23:51 -0700

    now with counting of clicks

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 
 /* jshint esversion: 6 */
 (function() {
-  var myHeaders = new Headers();
+
+  const catImage = document.getElementsByClassName('cat-image')[0];
+  const clickCount = document.getElementsByClassName('click-count')[0];
+
+  catImage.addEventListener('click', () => {
+    updateCatPicture();
+    clickCount.textContent = parseInt(clickCount.textContent) + 1;
+  });
 
   function parseCatURL(rawXML) {
     // create an object model from the XML returned,
@@ -11,7 +18,14 @@
     return xDOM.getElementsByTagName("url")[0].textContent;
   }
 
-  fetch('http://thecatapi.com/api/images/get?format=xml&results_per_page=1&size=small')
-    .then(response => { return response.text();})
-    .then(xml => console.log(parseCatURL(xml)));
+  function updateCatPicture() {
+    fetch('http://thecatapi.com/api/images/get?format=xml&results_per_page=1&size=small')
+      .then(response => { return response.text();})
+      .then(xml => {
+        catImage.src = parseCatURL(xml);
+    });
+  }
+
+  updateCatPicture();
+
 })();
