3b4c17e4fcfe1a62a9f7de5ce829470b7c5842d6
Russ Nealis <codemunkee@gmail.com>
2017-10-14 17:01:25 -0700

    now creating new DOMs from the Cat APIs xml responses

--- app.js
+++ app.js
@@ -1,26 +1,17 @@
 
 /* jshint esversion: 6 */
 (function() {
-  console.log('hi');
   var myHeaders = new Headers();
 
-  fetch('http://thecatapi.com/api/images/get?format=xml&results_per_page=20')
-    .then(response => {
-      let reader = response.body.getReader();
-      let bytesReceived = 0;
+  function parseCatURL(rawXML) {
+    // create an object model from the XML returned,
+    // and retreive the URL for the cat picture
+    const parser = new DOMParser();
+    const xDOM = parser.parseFromString(rawXML, 'text/xml');
+    return xDOM.getElementsByTagName("url")[0].textContent;
+  }
 
-      reader.read().then(function processResult(result) {
-        if (result.done) {
-          console.log("Fetch complete");
-          console.log(result);
-          return;
-        }
-
-        bytesReceived += result.value.length;
-        console.log('Received', bytesReceived, 'bytes of data so far.');
-
-        return reader.read().then(processResult);
-      });
-    })
-    .then(console.log('foo'));
+  fetch('http://thecatapi.com/api/images/get?format=xml&results_per_page=1&size=small')
+    .then(response => { return response.text();})
+    .then(xml => console.log(parseCatURL(xml)));
 })();
