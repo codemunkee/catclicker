c75562724b3f9c291a27640b20a87e7ea4536a7c
Russ Nealis <codemunkee@gmail.com>
2017-10-16 19:45:19 -0700

    updated to show a menu of multiple cats

--- app.js
+++ app.js
@@ -2,13 +2,6 @@
 /* jshint esversion: 6 */
 (function() {
 
-  const catImages = document.getElementsByClassName('cat-image');
-  const clickCount = document.getElementsByClassName('click-count')[0];
-
-  for (let catImage of catImages) {
-    catImage.addEventListener('click', handleCatClick);
-  }
-
   function incrementClick(elCatImage) {
     // go through nodes in cat image's parent element
     for (let node of elCatImage.parentNode.children) {
@@ -19,7 +12,13 @@
     }
   }
 
-  function handleCatClick() {
+  function handleCatLinkClick() {
+    hideAllCats();
+    let cat = document.getElementById(this.dataset.cat);
+    cat.style.display = "block";
+  }
+
+  function handleCatImageClick() {
       updateCatPicture(this);
       incrementClick(this);
   }
@@ -34,9 +33,37 @@
 
   function updateCatPicture(catImage) {
     fetch('https://thecatapi.com/api/images/get?format=xml&results_per_page=1&size=small')
-      .then(response => { return response.text();})
+      .then(response => {
+        catImage.src = 'loading.gif';
+        return response.text();
+      })
       .then(xml => {
         catImage.src = parseCatURL(xml);
       });
   }
+
+  function hideAllCats() {
+    const catBoxes = document.getElementsByClassName('cat-image-box');
+    for (const box of catBoxes) {
+      box.style.display = "none";
+    }
+  }
+
+  function showCatBox(catBox) {
+      catBox.style.display = "box";
+  }
+
+  const catImages = document.getElementsByClassName('cat-image');
+  const catLinks = document.getElementsByClassName('cat-link');
+  const clickCount = document.getElementsByClassName('click-count')[0];
+
+  for (let catLink of catLinks) {
+    catLink.addEventListener('click', handleCatLinkClick);
+  }
+
+  for (let catImage of catImages) {
+    updateCatPicture(catImage);
+    catImage.addEventListener('click', handleCatImageClick);
+  }
+
 })();
